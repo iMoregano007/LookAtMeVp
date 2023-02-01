b4e618c52bf4fede1e9f06b732de5e30a19675fa
iMoregano007 <imgauravs84@gmail.com>
2023-02-01 23:30:56 +0530

    finished project

--- app/src/main/java/com/example/lookatmevp/MainActivity.java
+++ app/src/main/java/com/example/lookatmevp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 
 import com.pd.lookatme.LookAtMe;
 
@@ -21,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
         lookAtMe.init(this);
         lookAtMe.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.nature));
         // lookAtMe.setVideoPath("http://website.com/video/mp4/62000/62792m.mp4"); to use video from a url
-
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(lookAtMe);
+        lookAtMe.setMediaController(mediaController);
         lookAtMe.start();
         lookAtMe.setLookMe();
     }
